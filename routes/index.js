11293a24f619671167b717bc018926f9ab5fcd45
Ty Connors <tconn89@gmail.com>
2017-01-25 01:32:40 -0500

    syntax error

--- routes/index.js
+++ routes/index.js
@@ -49,12 +49,14 @@
       return res.send('session undefined, you are not authorized')
 
     var my_cookie;
+    console.log(`auth: ${req.headers.authorization}`);
+    console.log(`my_cookie: ${req.headers.my_cookie}`);
     if(req.headers.authorization)
       my_cookie = req.headers.authorization.split(',');
     else if(req.headers.my_cookie)
       my_cookie = req.headers.my_cookie.split(',');
     else
-      return res.status.send('no cookie data, you are not authorized')
+      return res.status(401).send('no cookie data, you are not authorized')
 
     Session.findOne({secret: my_cookie[0]}, function(err,db_sesh){
       if(err)
@@ -176,6 +178,7 @@
     failureRedirect: '/login',
     failureFlash: true
   }), function(req, res, next) {
+    console.log(req.session.id);
     Session.findOne({secret: req.session.id}, function(err,doc){
       if(err)
         console.error(err);
